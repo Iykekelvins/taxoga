43a634e448d83f05c9ae98fe81dea112c2502678
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-02-12 13:39:36 +0100

    chore: update chat widget component

--- _pages/homepage/chat-widget.tsx
+++ _pages/homepage/chat-widget.tsx
@@ -4,7 +4,7 @@ export default function ChatWidget() {
 	return (
 		<button
 			className='bg-[linear-gradient(180deg,rgba(0,31,63,0.7)0%,rgba(0,51,102,0.7)100%)]
-    h-[max(3.125rem,50px)] rounded-full px-[max(0.75rem,12px)] w-fit flex items-center
+    h-[max(3.125rem,50px)] rounded-full px-[max(0.75rem,12px)] w-fit hidden md:flex items-center
     gap-2.5 fixed bottom-10 right-20
     '>
 			<Image
