8dd3586bf7e5ebc1933876df0d12d0a0c6fe1c4e
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-02-12 09:25:47 +0100

    chore: update tag component styles

--- components/tag.tsx
+++ components/tag.tsx
@@ -8,7 +8,11 @@ export default function Tag({ children, variant = 'white' }: TagProps) {
 		<span
 			className={`flex items-center justify-center h-[max(2.188rem,35px)] rounded-full
        px-[max(1rem,16px)] text-14 font-semibold w-fit
-       ${variant === 'white' ? 'bg-white text-black' : ''}
+       ${
+					variant === 'white'
+						? 'bg-white text-black'
+						: 'text-white bg-[linear-gradient(180deg,#001F3F_0%,#003366_100%),linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2))]'
+				}
       `}>
 			{children}
 		</span>
